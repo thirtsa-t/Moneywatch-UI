0c7d016c6ebb2a8954a569d98a21ad276b59ffee
Consolatrice-tech <bconso7@gmail.com>
2021-06-01 23:47:06 +0200

    Modifications in mainlayout

--- src/Components/MainLayout.js
+++ src/Components/MainLayout.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Layout, Menu } from 'antd';
 import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
+
   UserOutlined,
+  MenuOutlined,
   VideoCameraOutlined,
   UploadOutlined,
 } from '@ant-design/icons';
 
 const { Header, Sider, Content } = Layout;
 const MainLayout=({children}) =>{
+    const [collapsed,setCollapsed]= useState(false)
+  const toggle = () => {
+ setCollapsed(!collapsed)
+  };
 
     return (
       <Layout>
-        <Sider >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              transaction
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              bugdet
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              Report
-            </Menu.Item>
+        <Sider trigger={null} collapsible collapsed={collapsed} width="12vh" style={{minHeight: "200vh", position:"fixed"}}>
+          <div className="siderIcon" />
+
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
+          <Menu.Item key="1" icon={<MenuOutlined style={{fontSize:"15px"}} />}></Menu.Item>
+
+            <Menu.Item key="2" icon={<UserOutlined style={{fontSize:"15px"}}/>}>
+              
+            </Menu.Item><p style={{fontSize:"12px", marginTop:"-15px", marginLeft:"10px"}}>Transaction</p>
+
+            <Menu.Item key="3" icon={<VideoCameraOutlined style={{fontSize:"15px"}}/>}>
+            </Menu.Item><p style={{fontSize:"12px", marginTop:"-15px", marginLeft:"16px"}}>Budget</p>
+
+            <Menu.Item key="4" icon={<UploadOutlined style={{fontSize:"15px"}}/>}>
+            </Menu.Item><p style={{fontSize:"12px", marginTop:"-15px", marginLeft:"16px"}}>Report</p>
+
           </Menu>
         </Sider>
         <Layout className="site-layout">
