fd5136aefb62891072d90b9c5fdfb8abe442c003
thirtsa-t <thirtsaisimbi@gmail.com>
2021-06-01 00:18:47 -0700

    about home page

---
+++ moneywatchproject/moneywatchproject/src/Components/HomePage.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import image from '../Assets/image.jpg'
+import './index.css';
+
+
+const contentStyle = {
+   
+  };
+  
+const Homepage=()=>{
+    return( 
+        <div  className=' home-layout'>
+            
+            
+            <h1 style={{ margin: '3% 0% 0% 8%', font: '15px',color:'white' }}>Money watch
+            
+             </h1>
+             <h1 className='home'>English</h1>
+             <p className='middle-paragraphy'>MoneyWatch</p>
+             <p1 className='about-paragraphy'>“LET YOUR DREAM COMES  AND PLANS COMES TRUE”</p1>
+             
+  
+  </div>
+   
+
+
+  )
+  }
+  
+
+            
+
+            
+            
+          
+export default Homepage;
