3dab620c81c0122eeaa77aee61ce545a958b01a7
Consolatrice-tech <bconso7@gmail.com>
2021-06-02 21:57:29 +0200

    Edited mainlyout icons

--- src/Components/MainLayout.js
+++ src/Components/MainLayout.js
@@ -1,14 +1,21 @@
 import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Button} from 'antd';
 import {
 
-  UserOutlined,
+  SearchOutlined,
+  GithubOutlined,
+  LinkedinFilled,
+  TwitterOutlined,
+  SlackSquareOutlined, 
+  WalletOutlined,
   MenuOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
+  MoneyCollectOutlined, 
+  QuestionCircleFilled,
+  
 } from '@ant-design/icons';
 
 const { Header, Sider, Content } = Layout;
@@ -20,27 +27,41 @@ const MainLayout=({children}) =>{
 
     return (
       <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed} width="12vh" style={{minHeight: "200vh", position:"fixed"}}>
+        <Sider className='ant-menu' trigger={null} collapsible collapsed={collapsed} width="10vh" 
+        style={{minHeight: "200vh", position:"fixed", backgroundColor:"white"}}>
           <div className="siderIcon" />
 
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
+          <Menu mode="inline" defaultSelectedKeys={['0']}>
           <Menu.Item key="1" icon={<MenuOutlined style={{fontSize:"15px"}} />}></Menu.Item>
 
-            <Menu.Item key="2" icon={<UserOutlined style={{fontSize:"15px"}}/>}>
-              
-            </Menu.Item><p style={{fontSize:"12px", marginTop:"-15px", marginLeft:"10px"}}>Transaction</p>
+            <Menu.Item key="2" icon={<WalletOutlined style={{fontSize:"18px", marginTop:"20px"}}/>}>
+            </Menu.Item><p style={{fontSize:"10px", marginTop:"5px", marginLeft:"10px"}}>Transaction</p>
 
-            <Menu.Item key="3" icon={<VideoCameraOutlined style={{fontSize:"15px"}}/>}>
-            </Menu.Item><p style={{fontSize:"12px", marginTop:"-15px", marginLeft:"16px"}}>Budget</p>
+            <Menu.Item key="3" icon={<MoneyCollectOutlined style={{fontSize:"18px", marginTop:"20px"}}/>}>
+            </Menu.Item><p style={{fontSize:"10px", marginTop:"5px", marginLeft:"16px"}}>Budget</p>
 
-            <Menu.Item key="4" icon={<UploadOutlined style={{fontSize:"15px"}}/>}>
-            </Menu.Item><p style={{fontSize:"12px", marginTop:"-15px", marginLeft:"16px"}}>Report</p>
+            <Menu.Item key="4" icon={<QuestionCircleFilled  style={{fontSize:"18px", marginTop:"20px"}}/>}>
+            </Menu.Item><p style={{fontSize:"10px", marginTop:"5px", marginLeft:"16px"}}>Report</p>
 
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
           
+          <Header className="header-layout" width="10vh" style={{ padding: 0, backgroundColor:"white" }}>
+          <div className="headerIcon">
+          <Button type="primary" htmlType="link" className="head-button"> Add Transaction
+          </Button>
+          <div className="socialMediaAllIcon">
+            <menu theme="dark" mode="horizontal" defaultSelectedKeys={['0']}>
+            <GithubOutlined className="socialMediaIcon"/>
+            <LinkedinFilled className="socialMediaIcon"/>
+            <TwitterOutlined className="socialMediaIcon" />
+            <SlackSquareOutlined className="socialMediaIcon" />
+            <SearchOutlined className="searchIcon"/>
+            </menu>
+            </div>
+          <SearchOutlined className="searchIcon"/>
+          </div> 
           </Header>
           <Content
             className="site-layout-background"
