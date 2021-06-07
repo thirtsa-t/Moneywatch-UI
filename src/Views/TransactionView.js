1dff28a076cdf4a459647789953be992f43c925d
thirtsa-t <thirtsaisimbi@gmail.com>
2021-06-07 06:04:05 -0700

    about transaction

---
+++ src/Views/TransactionView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Report from '../Components/Report';
+import Transactions from '../Components/Transactions';
+
+
+const TransactionView  =()=>{
+    return(
+        <div>
+            <Transactions></Transactions>
+        </div>
+    )
+}
+export default TransactionView;
