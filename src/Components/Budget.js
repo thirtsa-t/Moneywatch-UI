69fd0668069b34d9e2b27d217cd579c3f9805f0e
unknown <tetaracheal@gmail.com>
2021-06-15 12:25:20 +0200

    editing budget and report

--- src/Components/Budget.js
+++ src/Components/Budget.js
@@ -2,13 +2,49 @@ import React ,{useState}from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Table, Button, Space,Card,Tabs } from 'antd';
+import { Table, Button, Space,Card,Tabs,Form,
+  Modal } from 'antd';
 import dataExpense from '../Assets/data/bugdeteExpenses.json';
 import dataIncome from '../Assets/data/budgetIncome.json';
-import dataSaving from '../Assets/data/budgetSavings.json'
+import dataSaving from '../Assets/data/budgetSavings.json';
+import AddBudget from './addBudget';
 
 const { TabPane } = Tabs;
-
+const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
+  const [expand, setExpand] = useState(false);
+  const [form] = Form.useForm();
+  const getFields = () => {
+    const count = expand ? 10 : 6;
+    const children = [];
+    return children;
+  };
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+    <Modal
+      visible={visible}
+      title="Add Budget"
+      width="60%"
+      okText="Save"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <AddBudget/>
+    </Modal>
+  );
+};
 const Budget=()=>{
   const [filteredInfo,setFilteredInfo]=useState(null);
   const [sortedInfo, setSortedInfo]=useState(null);
@@ -53,12 +89,26 @@ const handleChange = (pagination,  sorter) => {
     setSortedInfo( sorter)
 
 };
+const [visible, setVisible] = useState(false);
+const onCreate = (values) => {
+  console.log("Received values of form: ", values);
+  setVisible(false);
+};
 
     return(
       
         <Card >   <div className="card-container">
 
-<Button type="primary" style={{float: 'right'}}>Add</Button>
+<Button type="primary" style={{float: 'right'}}  
+            onClick={() => {
+              setVisible(true);}}>Add</Button>
+<CollectionCreateForm
+            visible={visible}
+            onCreate={onCreate}
+            onCancel={() => {
+              setVisible(false);
+            }}
+          />
         <Tabs type="card">
           <TabPane tab="expenses" key="1">
 
--- src/Services/apiMoneyWatch.js
+++ src/Services/apiMoneyWatch.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
-import {notification} from 'antd';
+// import axios from 'axios';
+// import {notification} from 'antd';
 
-const basicUrl = "https://money-watch1.herokuapp.com/api/v1";
+// const basicUrl = "https://money-watch1.herokuapp.com/api/v1";
 
-class MoneyApi{
+// class MoneyApi{
 
-    async signupUser(data){
+//     async signupUser(data){
 
-        try {
-            // console.log(">>>>>>>>",data);
-            const response = await axios.post(basicUrl+"/moneywatch/user/signup",data);
+//         try {
+//             // console.log(">>>>>>>>",data);
+//             const response = await axios.post(basicUrl+"/moneywatch/user/signup",data);
 
-            console.log(response)
-            return response.data;
-        } catch (error) {
-            notification.error({message:"user failed to sign up"});
-        }
+//             console.log(response)
+//             return response.data;
+//         } catch (error) {
+//             notification.error({message:"user failed to sign up"});
+//         }
 
-    }
+//     }
 
-    async signinUser(data){
+//     async signinUser(data){
 
-try{
-const response = await axios.post(basicUrl+"/moneywatch/user/signin",data)
+// try{
+// const response = await axios.post(basicUrl+"/moneywatch/user/signin",data)
 
-return response.data;
-}catch(error){
+// return response.data;
+// }catch(error){
 
-            notification.error({message:"user failed to signin"});
-}
+//             notification.error({message:"user failed to signin"});
+// }
 
-    }
+//     }
 
-}
+// }
 
-export default new MoneyApi();
+// export default new MoneyApi();
